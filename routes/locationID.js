1b65dde71f10948f863474233c58d2fe5a1c4995
David Anderson <djathree92@gmail.com>
2020-09-25 12:00:28 -0400

    api fix

--- routes/locationID.js
+++ routes/locationID.js
@@ -6,9 +6,13 @@ const locationIDCtrl = require('../controllers/locationID');
 
 
 /*---------- Protected Routes ----------*/
-router.post('/', locationIDCtrl.getAttractionsForLocationName);
+router.use(require('../config/auth'));
+router.post('/', checkAuth, locationIDCtrl.getAttractionsForLocationName);
 
-
+function checkAuth(req, res, next) {
+    if (req.user) return next();
+    return res.status(401).json({msg: 'Not Authorized'});
+  }
 /*---------- Auth Checker ----------*/
 
 module.exports = router;
